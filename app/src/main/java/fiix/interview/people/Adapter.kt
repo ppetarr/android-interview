6b863d7a9e010ae40c6f7fe306239c4e2f3d49d8
Petar Puric <puric.petarr@gmail.com>
2020-03-06 13:23:51 -0500

    start

---
+++ app/src/main/java/fiix/interview/people/Adapter.kt
@@ -0,0 +1,24 @@
+package fiix.interview.people
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class Adapter(): RecyclerView.Adapter<Adapter.ViewHolder>() {
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): Adapter.ViewHolder {
+
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(p0: Adapter.ViewHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
--- app/src/main/java/fiix/interview/people/MainActivity.kt
+++ app/src/main/java/fiix/interview/people/MainActivity.kt
@@ -2,6 +2,7 @@ package fiix.interview.people
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +13,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val recyclerView=findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter=
 
     }
 
